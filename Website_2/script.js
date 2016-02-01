30d5eeb885c88cb16456a2fbfd20b1e6d399844d
eichners <eichnersara@gmail.com>
2016-02-01 16:12:56 -0500

    trying to add all files for assignment

---
+++ Website_2/script.js
@@ -0,0 +1,16 @@
+/*
+loop listing favorite things, or favorite vegetables to grow for batman's farmstand instead
+*/
+
+/* var For Each
+*/
+var vegetableArray = ["eggplant","beans","kale","chard","cucumber","pepper","fennel","purple cherokee tomatoes"];
+
+function printValue(value) {
+	console.log("array item",value);
+}
+vegetableArray.forEach(printValue);
+vegetableArray.forEach(printValue);
+
+
+
